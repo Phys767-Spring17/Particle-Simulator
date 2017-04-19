e63e9671e8da58105292eff76cb441b2241fa1ad
anisak000 <anisak000@citymail.cuny.edu>
2017-04-19 01:29:30 -0400

    Problems with assert

--- Quarks/test_quark.py
+++ Quarks/test_quark.py
@@ -4,20 +4,19 @@ import pytest
 t = Quark()
 
 def test_quark_1():
-    assert if self.flavor == "up":
-            self.flavor = "down"
+    assert t.flip("up") == "down"
 
 def test_quark_2():
-    assert t.flip(down) == up
+    assert t.flip("down") == "up"
 
 def test_quark_3():
-    assert t.flip(top) == bottom
+    assert t.flip("top") == "bottom"
 
 def test_quark_4():
-    assert t.flip(bottom) == top
+    assert t.flip("bottom") == "top"
 
 def test_quark_5():
-    assert t.flip(strange) == charm
+    assert t.flip("strange") == "charm"
 
 def test_quark_6():
-    assert t.flip(charm) == strange
+    assert t.flip("charm") == "strange"
